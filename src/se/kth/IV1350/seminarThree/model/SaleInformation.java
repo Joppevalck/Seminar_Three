ff079a206687c100abc3a608d49c86bf20d95278
Johannesv <johannesvalck@gmail.com>
2020-04-25 15:36:49 +0200

    SaleInformation Complete

--- src/se/kth/IV1350/seminarThree/model/SaleInformation.java
+++ src/se/kth/IV1350/seminarThree/model/SaleInformation.java
@@ -1,19 +1,30 @@
 package se.kth.IV1350.seminarThree.model;
 
 import java.time.LocalTime;
+import java.util.HashMap;
 import java.util.Map;
 
-public class SaleInformation {
+/**
+ * SaleInformation contains all Information of a sale.
+ */
+class SaleInformation {
     private LocalTime saleTime;
     private StoreLocation storeLocation;
-    private Map items;
+    private Map itemInventory;
+    private double VAT;
+    private int runningTotal;
+    private int amountPaid;
+    private int change;
+
 
     /**
-     *
+     * Creates an instance of SaleInformation. Initializes the sale information.
      */
-    public SaleInformation(){
+    SaleInformation(){
         setTimeOfSale();
         setStoreLocationOfSale("nameOfStore", "addressOfStore" );
+        createItemInventory();
+        initMoneyVariables();
     }
 
     private void setTimeOfSale(){
@@ -22,6 +33,14 @@ public class SaleInformation {
 
     private void setStoreLocationOfSale(String nameOfStore, String addressOfStore){
         this.storeLocation = new StoreLocation(nameOfStore, addressOfStore);
+    }
+
+    private void createItemInventory(){
+        this.itemInventory = new HashMap<String, ItemAndQuantity>();
+    }
+
+    private void initMoneyVariables(){
+        this.runningTotal = this.amountPaid = this.change = 0;
     }
 
 }
